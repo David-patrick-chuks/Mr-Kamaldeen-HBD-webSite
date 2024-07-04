931097647f8a712f407f83af44fa8a1c162f289e
David-patrick-chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-07-04 13:48:14 +0100

    last commite

--- src/Card.jsx
+++ src/Card.jsx
@@ -7,8 +7,8 @@ import { IoPaperPlaneOutline } from 'react-icons/io5'
 import { BiComment } from 'react-icons/bi'
 export const Card = () => {
     return (
-        <div className='w-screen h-screen lg:h-fit lg:w-fit lg:p2 p-2 flex items-center justify-center '>
-            <div className=' shadow-md lg:w-1/3 lg:h-full rounded-xl bg-slate-200'>
+        <div className='w-screen h-screen lg:h-fit overflow-hidden lg:w-fit lg:p2 p-2 flex items-center justify-center '>
+            <div className=' shadow-md lg:w-3/12 lg:h-[95vh] overflow-hidden rounded-xl bg-slate-200'>
                 <div className='lg:w-[100%] px-4 pt-3 pb-2 flex  items-center justify-between'>
                     <div className='flex gap-[6px]'>
                         <img src={chuksFace} className=' rounded-full w-6 h-6 border border-white outline outline-yellow-800' />
